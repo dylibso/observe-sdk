d964d2852a80421efbf21085f36161b94853f2f1
Aaron Lee <wwkeyboard@users.noreply.github.com>
2023-06-06 15:52:26 -0500

    feat: Otel stdout adapter (#2)
    * feat: Otel stdout adapter
    
    The tokio aware version of the otel stdout adapter didn't make the
    move between repos. Here it is.
    
    * ci: add test for otel stdout adapter (#3)
    
    ---------
    
    Co-authored-by: Steve Manuel <steve@dylib.so>

---
+++ examples/otel-stdout.rs
@@ -0,0 +1,61 @@
+use dylibso_observe_sdk::{
+    adapter::{otelstdout::OtelStdoutAdapter, Collector},
+    add_to_linker,
+};
+use tokio::task;
+
+#[tokio::main]
+pub async fn main() -> anyhow::Result<()> {
+    let args: Vec<_> = std::env::args().skip(1).collect();
+    let data = std::fs::read(&args[0])?;
+    let function_name = "_start";
+    let config = wasmtime::Config::new();
+
+    // Create instance
+    let engine = wasmtime::Engine::new(&config)?;
+    let module = wasmtime::Module::new(&engine, data)?;
+
+    // let adapter = StdoutAdapter::new();
+    let adapter = OtelStdoutAdapter::new();
+
+    // Setup WASI
+    let wasi_ctx = wasmtime_wasi::WasiCtxBuilder::new()
+        .inherit_env()?
+        .inherit_stdio()
+        .args(&args.clone())?
+        .build();
+
+    let mut store = wasmtime::Store::new(&engine, wasi_ctx);
+    let mut linker = wasmtime::Linker::new(&engine);
+    wasmtime_wasi::add_to_linker(&mut linker, |wasi| wasi)?;
+
+    // Provide the observability functions to the `Linker` to be made available
+    // to the instrumented guest code. These are safe to add and are a no-op
+    // if guest code is uninstrumented.
+    let id = adapter.lock().await.new_collector();
+    let events = add_to_linker(id, &mut linker)?;
+
+    let collector = Collector::new(adapter, id, events).await?;
+
+    let instance = linker.instantiate(&mut store, &module)?;
+
+    // get the function and run it, the events pop into the queue
+    // as the function is running
+
+    let f = instance
+        .get_func(&mut store, function_name)
+        .expect("function exists");
+
+    OtelStdoutAdapter::start_trace(
+        String::from("module-name"),
+        function_name.to_string(),
+        || {
+            f.call(&mut store, &[], &mut []).unwrap();
+        },
+    );
+
+    task::yield_now().await;
+    collector.shutdown().await;
+
+    Ok(())
+}
