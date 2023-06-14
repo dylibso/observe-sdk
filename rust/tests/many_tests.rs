701468834eb0dabb064828828b8c3ff35fb9453e
Aaron Lee <aaron@dylibso.com>
2023-06-14 15:54:13 -0500

    feat: split up integration tests to their own files

--- rust/tests/many_tests.rs
+++ rust/tests/many_tests.rs
@@ -1,36 +1,37 @@
 #[cfg(test)]
 mod tests {
-    use std::process::Command;
-
     use anyhow::Result;
+    use std::process::Command;
+    use std::thread;
+    use std::time;
 
     #[test]
-    fn integration_basic() -> Result<()> {
-        // cargo run --example basic ../test/test.c.instr.wasm
+    fn integration_many() -> Result<()> {
+        // cargo run --example many ../test/test.c.instr.wasm
         let output = Command::new("cargo")
-            .args(&["run", "--example", "basic", "../test/test.c.instr.wasm"])
+            .args(&["run", "--example", "many", "../test/test.c.instr.wasm"])
             .output()
             .expect("Failed to run the example `examples/basic`");
-
+        thread::sleep(time::Duration::from_millis(150));
         let output = String::from_utf8(output.stdout)?;
         let output_lines = output.lines();
-
+        println!("{}", output);
         // First test that the expected output was printed 10 times
         assert_eq!(
             output_lines
                 .clone()
                 .filter(|l| l.contains("Hello, world!"))
                 .count(),
-            10
+            1000
         );
 
         // Check that printf was also called 10 times
         assert_eq!(
             output_lines
                 .clone()
-                .filter(|l| l.ends_with("Func: printf"))
+                .filter(|l| l.contains("printf"))
                 .count(),
-            10
+            100
         );
 
         Ok(())
