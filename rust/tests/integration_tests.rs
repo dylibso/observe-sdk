e9a1d3c84a44ae85341706beaf899fed21037202
Aaron Lee <wwkeyboard@users.noreply.github.com>
2023-06-15 13:10:09 -0500

    feat: Integration test runner that tests the examples (#17)

---
+++ rust/tests/integration_tests.rs
@@ -0,0 +1,38 @@
+#[cfg(test)]
+mod tests {
+    use std::process::Command;
+
+    use anyhow::Result;
+
+    #[test]
+    fn integration_basic() -> Result<()> {
+        // cargo run --example basic ../test/test.c.instr.wasm
+        let output = Command::new("cargo")
+            .args(&["run", "--example", "basic", "../test/test.c.instr.wasm"])
+            .output()
+            .expect("Failed to run the example `examples/basic`");
+
+        let output = String::from_utf8(output.stdout)?;
+        let output_lines = output.lines();
+
+        // First test that the expected output was printed 10 times
+        assert_eq!(
+            output_lines
+                .clone()
+                .filter(|l| l.contains("Hello, world!"))
+                .count(),
+            10
+        );
+
+        // Check that printf was also called 10 times
+        assert_eq!(
+            output_lines
+                .clone()
+                .filter(|l| l.ends_with("Func: printf"))
+                .count(),
+            10
+        );
+
+        Ok(())
+    }
+}
---
+++ rust/tests/otel_json_tests.rs
@@ -0,0 +1,59 @@
+#[cfg(test)]
+mod tests {
+    use std::convert::identity;
+    use std::process::Command;
+
+    use anyhow::Result;
+    use serde_json::Value;
+
+    mod helpers;
+    use helpers::otel_json::*;
+
+    #[test]
+    fn otel_stdout() -> Result<()> {
+        // cargo run --example otel-stdout ../test/test.c.instr.wasm
+        let output = Command::new("cargo")
+            .args(&[
+                "run",
+                "--example",
+                "otel-stdout",
+                "../test/test.c.instr.wasm",
+            ])
+            .output()
+            .expect("Failed to run the example `examples/basic`");
+
+        let output = String::from_utf8(output.stdout)?;
+
+        // traces is the collection of all traces emitted from this run
+        let traces = output
+            .lines()
+            .map(|l| match serde_json::from_str(l) {
+                Ok(x) => Some(x),
+                Err(_) => None,
+            })
+            .collect::<Vec<Option<Value>>>()
+            .into_iter()
+            .filter_map(identity)
+            .collect::<Vec<Value>>();
+
+        let trace_id = attribute_of_first_span(traces.first().unwrap(), "traceId".to_string());
+        assert_eq!(trace_id, Some("any-old-trace-id".to_string()));
+
+        // test.c.instr.wasm spits out 10 allocations at the top level, this may change with the
+        // function naming work
+        let allocations = traces
+            .iter()
+            .filter(|t| attribute_of_first_span(t, "name".to_string()).unwrap() == "allocation");
+        assert_eq!(allocations.count(), 10);
+
+        // We know the 11th trace emitted is the first function call
+        let (ids, parent_ids) = ids_and_parent_span_ids(traces.get(11).unwrap());
+
+        // the first span won't have a parent in our list
+        for parent_id in &parent_ids[1..] {
+            assert!(ids.contains(&parent_id));
+        }
+
+        Ok(())
+    }
+}
