aa46cf914ea3adaaa243c63184267b9e491e4613
Benjamin Eckel <bhelx@simst.im>
2023-07-27 14:36:57 -0500

    demonstrate manual instrumentation

--- test/kitchensink.c
+++ test/kitchensink.c
@@ -12,6 +12,16 @@ IMPORT("dylibso_observe", "span_enter")
 extern void span_enter(uint32_t, uint32_t);
 IMPORT("dylibso_observe", "span_exit") extern void span_exit();
 
+void custom_span_enter(char name[]) {
+  uintptr_t ptr = (uintptr_t)name;
+  uint64_t uint64_ptr = (uint64_t)ptr;
+  uint64_t uint64_length = (uint64_t)(strlen(name));
+
+  span_enter(uint64_ptr, uint64_length);
+}
+
+void custom_span_exit() { span_exit(); }
+
 void write_stat() {
   char stat[] = "vowels.count:1|c";
 
@@ -19,7 +29,9 @@ void write_stat() {
   uint64_t uint64_ptr = (uint64_t)ptr;
   uint64_t uint64_length = (uint64_t)(strlen(stat));
 
+  custom_span_enter("statsd");
   statsd(uint64_ptr, uint64_length);
+  custom_span_exit();
 }
 
 void write_log() {
@@ -30,33 +42,29 @@ void write_log() {
   uint64_t uint64_length = (uint64_t)(strlen(stat));
   uint64_t level = (uint64_t)1;
 
+  custom_span_enter("log_write");
   log_write(level, uint64_ptr, uint64_length);
-}
-
-void custom_span() {
-  char name[] = "myCustomFunctionSpan";
-
-  uintptr_t ptr = (uintptr_t)name;
-  uint64_t uint64_ptr = (uint64_t)ptr;
-  uint64_t uint64_length = (uint64_t)(strlen(name));
-
-  span_enter(uint64_ptr, uint64_length);
-  span_enter(uint64_ptr, uint64_length);
-  span_enter(uint64_ptr, uint64_length);
-  span_exit();
-  span_exit();
-  span_exit();
+  custom_span_exit();
 }
 
 void run() {
+  custom_span_enter("printf");
   printf("Hello from Wasm!\n");
-  custom_span();
+  custom_span_exit();
+
+  custom_span_enter("write_stat");
   write_stat();
+  custom_span_exit();
+
+  custom_span_enter("write_log");
   write_log();
+  custom_span_exit();
 }
 
 int main(int argc, char *argv[]) {
+  custom_span_enter("run");
   run();
+  custom_span_exit();
 
   return 0;
 }
