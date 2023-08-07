d05f580386f6a7482a62cfa2e6b45eacdb6f2cde
Gavin Hayes <gavin@dylibso.com>
2023-08-07 17:39:25 -0400

    chore: add observe-api build in c++, improve styling (#81)

--- observe-api/c/observe_api.c
+++ observe-api/c/observe_api.c
@@ -1,26 +1,26 @@
 #include "observe_api.h"
-#include <inttypes.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void span_enter(char *name) {
-  uint64_t uint64_ptr = (uint64_t)name;
-  uint32_t uint32_length = (uint32_t)(strlen(name));
+void span_enter(const char *name) {
+  const uint64_t uint64_ptr = (uint64_t)name;
+  const uint32_t uint32_length = strlen(name);
   _span_enter(uint64_ptr, uint32_length);
 }
 
-void span_exit() { _span_exit(); }
+void span_exit(void) { _span_exit(); }
 
-void metric(char *metric) {
-  uint64_t uint64_ptr = (uint64_t)metric;
-  uint32_t uint32_length = (uint32_t)(strlen(metric));
-  _metric((uint64_t)1, uint64_ptr, uint32_length);
+void metric(const char *metric) {
+  const uint64_t uint64_ptr = (uint64_t)metric;
+  const uint32_t uint32_length = strlen(metric);
+  _metric(1, uint64_ptr, uint32_length);
 }
 
-void write_log(int level, char *msg) {
-  uint64_t uint64_ptr = (uint64_t)msg;
-  uint32_t uint32_length = (uint32_t)(strlen(msg));
-  uint32_t uint32_level = (uint32_t)level;
+void write_log(const enum DO_LOG_LEVEL level, const char *msg) {
+  const uint64_t uint64_ptr = (uint64_t)msg;
+  const uint32_t uint32_length = strlen(msg);
+  const uint32_t uint32_level = level;
   _log(uint32_level, uint64_ptr, uint32_length);
 }
