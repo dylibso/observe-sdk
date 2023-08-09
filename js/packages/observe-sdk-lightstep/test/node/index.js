37797f9b78b2e64ae3ea0c85005fc27edd3695f5
Rob Wong <rob@dylibso.com>
2023-08-09 12:44:12 -0600

    feat(js): Add Lightstep adapter

---
+++ js/packages/observe-sdk-lightstep/test/node/index.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const { WASI } = require("wasi");
+const { env, argv } = require('node:process');
+const { HoneycombAdapter } = require("@dylibso/observe-sdk-honeycomb");
+require('dotenv').config();
+
+const wasi = new WASI({
+  version: "preview1",
+  args: argv.slice(1),
+  env,
+});
+
+const config = {
+  apiKey: process.env.HONEYCOMB_API_KEY,
+  dataset: 'node',
+  emitTracesInterval: 1000,
+  traceBatchMax: 100,
+  host: 'https://api.honeycomb.io',
+}
+const adapter = new HoneycombAdapter(config);
+
+const bytes = fs.readFileSync("../../test-data/test.c.instr.wasm");
+adapter.start(bytes).then((traceContext) => {
+  const module = new WebAssembly.Module(bytes);
+
+  WebAssembly.instantiate(module, {
+    ...wasi.getImportObject(),
+    ...traceContext.getImportObject(),
+  }).then((instance) => {
+    wasi.start(instance);
+    // adapter.setMetadata({
+    //   http_status_code: 200,
+    //   http_url: "https://example.com",
+    // });
+    traceContext.stop();
+  });
+});
