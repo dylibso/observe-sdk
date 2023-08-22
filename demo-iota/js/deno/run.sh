97b06eab0222993042f83d2e72f4bc761eb56a95
Rob Wong <rob@dylibso.com>
2023-08-22 13:04:58 -0600

    fix: start from node image

--- demo-iota/js/deno/run.sh
+++ demo-iota/js/deno/run.sh
@@ -1,4 +1,4 @@
-deno run --allow-net --allow-write --allow-env --allow-read index.ts
+$HOME/.deno/bin/deno run --allow-net --allow-write --allow-env --allow-read index.ts
 
 # Wait for any process to exit
 wait -n
