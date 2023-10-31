924c5384e2856f0db313d1bfcf9f9cdd4ac7df52
Chris Dickinson <chris@dylibso.com>
2023-10-31 12:02:48 -0700

    fix: bump WASM_INSTR_VERSION_MINOR

--- rust/src/wasm_instr.rs
+++ rust/src/wasm_instr.rs
@@ -6,7 +6,7 @@ use anyhow::{bail, Result};
 // WASM_INSTR_VERSION_MAJOR must match the instrumented wasm
 // wasmInstrVersionMinor must be <= the value in instrumented wasm
 pub const WASM_INSTR_VERSION_MAJOR: u32 = 0;
-pub const WASM_INSTR_VERSION_MINOR: u32 = 0; // TODO: bump this to match compiler when ready
+pub const WASM_INSTR_VERSION_MINOR: u32 = 4; // TODO: bump this to match compiler when ready
 
 // Static info from instrumentation
 pub struct WasmInstrInfo {
