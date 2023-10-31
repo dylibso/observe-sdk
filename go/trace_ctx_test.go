b4a765d1f1479c7a8b54d0939f571d28e86ae93e
zach <zach@dylibso.com>
2023-10-31 13:06:48 -0700

    feat(go): Add `TraceCtx.SetTraceId` to allow re-using extisting trace IDs (#134)
    * feat: add TraceCtx.SetTraceId to set a custom traceid
    
    * test: add test for length of trace id

---
+++ go/trace_ctx_test.go
@@ -0,0 +1,27 @@
+package observe
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTraceId(t *testing.T) {
+	a := NewTraceId()
+
+	b := TelemetryId{}
+	if err := b.FromString(a.ToHex16()); err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println(a.ToHex16(), b.ToHex16())
+	fmt.Println(a, b)
+
+	if a != b {
+		t.Fail()
+	}
+
+	err := b.FromString(a.ToHex16() + "a")
+	if err == nil {
+		t.Fail()
+	}
+}
