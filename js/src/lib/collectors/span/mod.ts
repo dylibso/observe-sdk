388f35456d435489e9c0c56f8b46dbd805d8c761
Gavin Hayes <gavin@dylibso.com>
2023-11-17 15:22:55 -0500

    chore: js-sdk warn on discovery of deprecated namespace dylibso_observe

--- js/src/lib/collectors/span/mod.ts
+++ js/src/lib/collectors/span/mod.ts
@@ -48,6 +48,14 @@ export class SpanCollector implements Collector {
     mangledNames.forEach((value, key) => {
       this.names.set(key, demangle(value));
     });
+
+    for (const iName of WebAssembly.Module.imports(module)) {
+      if (iName.module === 'dylibso_observe') {
+        console.warn("Module uses deprecated namespace \"dylibso_observe\"!\n" +
+          "Please consider reinstrumenting with newer wasm-instr!");
+        break;
+      }
+    }
   }
 
   public send(to: Adapter): void {
