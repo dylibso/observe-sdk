63e45e4d3aa29539020d0382acd10b1723e36e47
Gavin Hayes <gavin@dylibso.com>
2024-07-11 18:34:07 -0400

    feat(c++ observe api): Span class

--- observe-api/c/observe_api.hpp
+++ observe-api/c/observe_api.hpp
@@ -9,11 +9,23 @@
 namespace observe_api {
 void span_enter(std::string_view name);
 void span_exit();
-void metric(enum DO_METRIC_FMT format, std::string_view metric);
+void metric(enum DO_METRIC_FMT format, std::string_view mtc);
 void log(enum DO_LOG_LEVEL level, std::string_view message);
 void span_tags(std::string_view tags);
-void statsd(std::string_view metric);
+void statsd(std::string_view mtc);
 void span_tags(std::vector<std::string> &tags);
+
+class Span {
+public:
+  Span(std::string_view name) { span_enter(name); }
+  ~Span() { span_exit(); }
+  void metric(enum DO_METRIC_FMT format, std::string_view mtc) {
+    observe_api::metric(format, mtc);
+  }
+  void tags(std::string_view tags) { span_tags(tags); }
+  void statsd(std::string_view mtc) { observe_api::statsd(mtc); }
+  void tags(std::vector<std::string> &tags) { span_tags(tags); }
+};
 }; // namespace observe_api
 
 #endif // OBSERVE_API_HPP
@@ -43,8 +55,8 @@ void span_enter(std::string_view name) {
 
 void span_exit() { observe_api_span_exit(); }
 
-void metric(enum DO_METRIC_FMT format, std::string_view metric) {
-  observe_api_metric_n(format, metric.data(), metric.size());
+void metric(enum DO_METRIC_FMT format, std::string_view mtc) {
+  observe_api_metric_n(format, mtc.data(), mtc.size());
 }
 
 void log(enum DO_LOG_LEVEL level, std::string_view message) {
@@ -55,8 +67,8 @@ void span_tags(std::string_view tags) {
   observe_api_span_tags_n(tags.data(), tags.size());
 }
 
-void statsd(std::string_view metric) {
-  observe_api_statsd_n(metric.data(), metric.size());
+void statsd(std::string_view mtc) {
+  observe_api_statsd_n(mtc.data(), mtc.size());
 }
 
 void span_tags(std::vector<std::string> &tags) {
